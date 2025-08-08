890a785d167a944a1ae5d483b8df8f549958f3ae
bolasunilkamath <64834672+bolasunilkamath@users.noreply.github.com>
2025-08-08 12:49:23 +0530

    Add files via upload

---
+++ Week1/server1.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const hostname = '172.16.11.113';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');      // Indicates the type of data sent to Client
+    if (req.url === '/') {
+    res.statusCode = 200;                           // Status Code OK
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+   res.write('Hello sunil kamath, World!');      
+   res.end();                 // End the Response with Hello, Node.js Server!
+  } //else 
+  if (req.url === '/about') {
+   res.statusCode = 200;
+   res.end('This is the About page.');
+  } else {
+  res.statusCode = 404;                           // Status Code Not Found!
+   res.end('Page not found');
+  }
+});
+
+server.listen(port, hostname, () => {             // START the server.
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
